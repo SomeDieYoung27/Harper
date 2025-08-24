a316d98165cdae61af779123ffe3117bc8f31baf
Shashwat Chaturvedi <111681395+SomeDieYoung27@users.noreply.github.com>
2025-08-24 17:03:59 +0530

    Feat :demo test function

---
+++ packages/harper-core/test/demo.test.ts
@@ -0,0 +1,11 @@
+
+function sum(a : number,b : number) {
+    return a + b;
+}
+
+describe('demo test',() => {
+    test("test: 1 + 2 = 3", () => {
+        Log.info("1+2=?");
+        expect(sum(1, 2)).toBe(3);
+      });
+})
