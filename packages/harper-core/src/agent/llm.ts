b36866fe90d97028276bb691ebffcf0cf8c79589
Shashwat Chaturvedi <111681395+SomeDieYoung27@users.noreply.github.com>
2025-09-09 02:05:06 +0530

    Feat : llm of the agent

--- packages/harper-core/src/agent/llm.ts
+++ packages/harper-core/src/agent/llm.ts
@@ -75,3 +75,466 @@ export function getTool<T extends Tool | DialogueTool>(
     }
     return null;
   }
+
+export function convertToolResult(
+  toolUse: LanguageModelV2ToolCallPart,
+  toolResult: ToolResult,
+  user_messages: LanguageModelV2Prompt
+) : LanguageModelV2ToolResultPart {
+  let result : LanguageModelV2ToolResultOutput;
+  if(!toolResult || !toolResult.content){
+    result = {
+      type: "error-text",
+      value: "Error",
+    };
+  } else if(toolResult.content.length == 1 && toolResult.content[0].type == "text"){
+    let text = toolResult.content[0].text;
+    result = {
+      type: "text",
+      value: text,
+    };
+    let isError = toolResult.isError == true;
+    if (isError && !text.startsWith("Error")) {
+      text = "Error: " + text;
+      result = {
+        type: "error-text",
+        value: text,
+      };
+    } else if(!isError && text.length == 0){
+      text = "Sucessfull";
+      result = {
+        type: "text",
+        value: text,
+      };
+    }
+    if(
+      text && 
+      ((text.startsWith("{") && text.endsWith("}")) ||
+        (text.startsWith("[") && text.endsWith("]")))
+    ) {
+      try {
+        result = JSON.parse(text);
+        result = {
+          type: "json",
+          value: result,
+        }
+      } catch (error) {
+      }
+
+    } else {
+      result = {
+        type: "content",
+        value: [],
+      };
+    }
+    for(let i= 0;i<toolResult.content.length;i++){
+      let content = toolResult.content[i];
+      if(content.type == "text"){
+        result.value.push({
+          type: "text",
+          text: content.text,
+        });
+      } else {
+        if(config.toolResultMultimodal){
+          let mediaData = content.data;
+          if(mediaData.startsWith("data:")){
+            mediaData = mediaData.substring(mediaData.indexOf(",") + 1);
+          }
+          result.value.push({
+            type: "media",
+            data: mediaData,
+            mediaType: content.mimeType || "image/png",
+          });
+        } else{
+          user_messages.push({
+            role: "user",
+            content: [
+              {
+                type: "file",
+                data: toFile(content.data),
+                mediaType: content.mimeType || getMimeType(content.data),
+              },
+              {
+                type: "text",
+                text: `call \`${toolUse.toolName}\` tool result`,
+              },
+            ],
+          });
+        }
+      }
+    }
+  }
+  return {
+    type: "tool-result",
+    toolCallId: toolUse.toolCallId,
+    toolName: toolUse.toolName,
+    output: result,
+  };
+}
+
+export async function callAgentLLM(
+  agentContext : AgentContext,
+  rlm: RetryLanguageModel,
+  messages: LanguageModelV2Prompt,
+  tools : LanguageModelV2FunctionTool[],
+  noCompress?: boolean,
+  toolChoice?: LanguageModelV2ToolChoice,
+  retryNum: number = 0,
+  callback?: StreamCallback & HumanCallback,
+  requestHandler?: (request: LLMRequest) => void
+) : Promise<Array<LanguageModelV2TextPart | LanguageModelV2ToolCallPart>> {
+
+  await AgentContext.context.checkAborted();
+  if(messages.length >= config.compressThreshold && !noCompress){
+    await memory.compressAgentMessages(AgentContext,rlm,messages,tools);
+  }
+  if(!toolChoice){
+    appendUserConversation(agentContext,messages);
+  }
+  const context = agentContext.context;
+  const agentChain = agentContext.agentChain;
+  const agentNode = agentChain.agent;
+
+  const streamCallback = callback || 
+          context.config.callback || {
+            onMessage: async () => {},
+          };
+
+  const stepController = new AbortController();
+  const signal = AbortSignal.any([
+    context.controller.signal,
+    stepController.signal,
+  ]);
+
+  const request : LLMRequest = {
+    tools: tools,
+    toolChoice,
+    messages: messages,
+    abortSignal: signal,
+  };
+
+  requestHandler && requestHandler(request);
+  let streamText = "";
+  let thinkText = "";
+  let toolArgsText = "";
+  let textStreamId = uuidv4();
+  let thinkStreamId = uuidv4();
+  let textStreamDone = false;
+
+  const toolParts: LanguageModelV2ToolCallPart[] = [];
+  let reader : ReadableStreamDefaultReader<LanguageModelV2StreamPart> | null =
+  null;
+  try {
+    agentChain.agentRequest = request;
+    context.currentStepControllers.add(stepController);
+    const result : StreamResult = await rlm.callStream(request);
+    reader = result.stream.getReader();
+    let toolPart: LanguageModelV2ToolCallPart | null = null;
+    while(true){
+      await context.checkAborted();
+      const { done, value } = await reader.read();
+      if(done){
+        break;
+      }
+      const chunk = value as LanguageModelV2StreamPart;
+      switch (chunk.type){
+        case "text-start" :{
+          textStreamId = uuidv4();
+          break;
+        }
+        case "text-delta" : {
+          if (toolPart && !chunk.delta) {
+            continue;
+          }
+          streamText += chunk.delta || "";
+          await streamCallback.onMessage(
+            {
+              taskId : context.taskId,
+              agentName: agentNode.name,
+              nodeId: agentNode.id,
+              type: "text",
+              streamId: textStreamId,
+              streamDone : false,
+              text: streamText,
+            },
+            agentContext
+          );
+          if(toolPart){
+            await streamCallback.onMessage(
+              {
+              taskId: context.taskId,
+              agentName: agentNode.name,
+              nodeId: agentNode.id,
+              type: "tool_use",
+              toolId: toolPart.toolCallId,
+              toolName: toolPart.toolName,
+              params: toolPart.input || {},
+              },
+              agentContext
+            );
+            toolPart = null;
+          }
+          break;
+        }
+        case "text-end" : {
+          textStreamDone = true;
+          if (streamText) {
+            await streamCallback.onMessage(
+              {
+                taskId: context.taskId,
+                agentName: agentNode.name,
+                nodeId: agentNode.id,
+                type: "text",
+                streamId: textStreamId,
+                streamDone: true,
+                text: streamText,
+              },
+              agentContext
+            );
+          }
+          break;
+        }
+        case "reasoning-start" : {
+          thinkStreamId = uuidv4();
+          break;
+        }
+        case "reasoning-delta" : {
+          thinkText += chunk.delta || "";
+          await streamCallback.onMessage(
+            {
+              taskId: context.taskId,
+              agentName: agentNode.name,
+              nodeId: agentNode.id,
+              type: "thinking",
+              streamId: thinkStreamId,
+              streamDone: false,
+              text: thinkText,
+            },
+            agentContext
+          );
+          break;
+        }
+      case "reasoning-end" : {
+        if(thinkText){
+          await streamCallback.onMessage(
+            {
+              taskId: context.taskId,
+              agentName: agentNode.name,
+              nodeId: agentNode.id,
+              type: "thinking",
+              streamId: thinkStreamId,
+              streamDone: true,
+              text: thinkText,
+            },
+            agentContext
+          );
+        }
+        break;
+      }
+      case "tool-input-start" :{
+        if (toolPart && toolPart.toolCallId == chunk.id) {
+
+        } else {
+          toolPart = {
+            type: "tool-call",
+            toolCallId: chunk.id,
+            toolName: chunk.toolName,
+            input: {},
+          };
+          toolParts.push(toolPart);
+        }
+        break;
+      }
+      case "tool-input-delta": {
+        if(!textStreamDone) {
+          textStreamDone = true;
+          await streamCallback.onMessage(
+            {
+              taskId: context.taskId,
+              agentName: agentNode.name,
+              nodeId: agentNode.id,
+              type: "text",
+              streamId: textStreamId,
+              streamDone: true,
+              text: streamText,
+            },
+            agentContext
+          );
+        }
+        toolArgsText += chunk.delta || "";
+        await streamCallback.onMessage(
+          {
+            taskId: context.taskId,
+            agentName: agentNode.name,
+            nodeId: agentNode.id,
+            type: "tool_streaming",
+            toolId: chunk.id,
+            toolName: toolPart?.toolName || "",
+            paramsText: toolArgsText,
+          },
+          agentContext
+        );
+        break;
+      }
+      case "tool-call" : {
+        toolArgsText = "";
+        const args = chunk.input ? JSON.parse(chunk.input) : {};
+        const message: StreamCallbackMessage = {
+          taskId: context.taskId,
+          agentName: agentNode.name,
+          nodeId: agentNode.id,
+          type: "tool_use",
+          toolId: chunk.toolCallId,
+          toolName: chunk.toolName,
+          params: args,
+        };
+        await streamCallback.onMessage(message,agentContext);
+        if(toolPart == null){
+          toolParts.push({
+            type: "tool-call",
+            toolCallId: chunk.toolCallId,
+            toolName: chunk.toolName,
+            input: message.params || args,
+          });
+        } else {
+          toolPart.input = message.params || args;
+          toolPart = null;
+        }
+        break;
+      }
+      case "file" : {
+        await streamCallback.onMessage(
+          {
+            taskId: context.taskId,
+            agentName: agentNode.name,
+            nodeId: agentNode.id,
+            type: "file",
+            mimeType: chunk.mediaType,
+            data: chunk.data as string,
+          },
+          agentContext
+        );
+        throw new Error("LLM Error: " + chunk.error);
+      }
+      case "finish" : {
+        if (!textStreamDone) {
+          textStreamDone = true;
+          await streamCallback.onMessage(
+            {
+              taskId: context.taskId,
+              agentName: agentNode.name,
+              nodeId: agentNode.id,
+              type: "text",
+              streamId: textStreamId,
+              streamDone: true,
+              text: streamText,
+            },
+            agentContext
+          );
+        }
+        if(toolPart){
+          await streamCallback.onMessage(
+            {
+              taskId: context.taskId,
+              agentName: agentNode.name,
+              nodeId: agentNode.id,
+              type: "tool_use",
+              toolId: toolPart.toolCallId,
+              toolName: toolPart.toolName,
+              params: toolPart.input || {},
+            },
+            agentContext
+          );
+          toolPart = null;
+        }
+        await streamCallback.onMessage(
+          {
+            taskId: context.taskId,
+              agentName: agentNode.name,
+              nodeId: agentNode.id,
+              type: "finish",
+              finishReason: chunk.finishReason,
+              usage: {
+                promptTokens: chunk.usage.inputTokens || 0,
+                completionTokens: chunk.usage.outputTokens || 0,
+                totalTokens:
+                  chunk.usage.totalTokens ||
+                  (chunk.usage.inputTokens || 0) +
+                    (chunk.usage.outputTokens || 0),
+              },
+          },
+          agentContext
+        );
+        if(
+          chunk.finishReason === "length" &&
+          messages.length >= 5 &&
+          !noCompress &&
+          retryNum < config.maxRetryNum
+        ) {
+          await memory.compressAgentMessages(
+            agentContext,
+            rlm,
+            messages,
+            tools
+          );
+          return callAgentLLM(
+            agentContext,
+            rlm,
+            messages,
+            tools,
+            noCompress,
+            toolChoice,
+            ++retryNum,
+            streamCallback
+          );
+        }
+        break;
+      }
+      }
+    }
+  } catch (error) {
+    await context.checkAborted();
+    if(retryNum < config.maxRetryNum) {
+      await sleep(300 * (retryNum + 1) * (retryNum + 1));
+      return callAgentLLM(
+        agentContext,
+        rlm,
+        messages,
+        tools,
+        noCompress,
+        toolChoice,
+        ++retryNum,
+        streamCallback
+      );
+    }
+    throw error;
+  } finally {
+    reader && reader.releaseLock();
+    context.currentStepControllers.delete(stepController);
+  }
+  agentChain.agentRequest = streamText;
+  return streamText ? [
+    
+      { type: "text", text: streamText } as LanguageModelV2TextPart,
+      ...toolParts,
+  ] : toolParts;
+
+}
+
+function appendUserConversation(agentContext : AgentContext, messages : LanguageModelV2Prompt) {
+  const userPrompts = agentContext.context.conversation.splice(0,agentContext.context.conversation.length).filter(
+    (s) => !!s
+  );
+  if(userPrompts.length > 0){
+    const prompt = "The user is intervening in the current task, please replan and execute according to the following instructions:\n" +
+      userPrompts.map((s) => `- ${s.trim()}`).join("\n");
+
+    messages.push({
+      role: "user",
+      content: [{ type: "text", text: prompt }],
+    });
+  }
+}
+
+
+
